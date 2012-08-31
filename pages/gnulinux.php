7827fb21886e90787b4c83ec18d2d49a2b6db868
ketsol <eddieyan101@gmail.com>
2012-08-31 01:36:15 -0700

    updates to GNU/Linux page

--- pages/gnulinux.php
+++ pages/gnulinux.php
@@ -19,8 +19,26 @@
 		<h6>Multiple Monitors</h6>
 			<p>Using xrandr seems to work nicely:</p>
 		<pre>xrandr --output [OutputOfMonitor] --[position]-of [OutputOfReferenceMonitor]</pre>
-
-
+	
+	<h3>Ubuntu</h3>
+		<h4>Changing default file manager</h4>
+		<p>This is important for issues such as nautilus being used in firefox when firefox is running in window manager that does not play nice with the idiotic feature of having to have a "desktop"
+		<br />
+		As root, edit <br />
+		<tt>/usr/share/applications/defaults.list</tt> <br />
+		and replace or modify the line
+	
+		<tt>inode/directory=[yourManager].desktop <br />
+		     x-directory/normal=[yourManager.desktop
+		</tt>
+		
+		</p>
+		<h5>Untested method:</h5>
+		<p>Use <br />
+			<tt>
+				exo-preferred-applications
+			</tt>
+		</p>
 
 </body>
 
---
+++ pages/vim.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Vim Notes</title>
+</head>
+
+<body>
+	<h6>This page created on August 31, 2012</h6>
+	<h2>
+</body>
+</html>
