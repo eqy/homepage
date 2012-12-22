27b0791597ed403879f19a547480df609206781d
ketsol <ketsol@ketchupserver.(none)>
2012-12-21 20:51:06 -0800

    Some stylistic changes to hardware page, updated information

--- main.php
+++ main.php
@@ -17,8 +17,5 @@ Note: If you are the domain you are on looks something like linux.ucla.edu, then
 I cannot forsee additions to this page, but I am very interested to what additions to this website entail. I may revive some older projects done in HTML/PHP, but at the very least, document technological adventures. 
 </p>
 
-<h6>This page created on August 27, 2012</h6>
-<h6>Last updated on August 28, 2012</h6>
-
 </body>
 </html>
--- pages/hardware.php
+++ pages/hardware.php
@@ -8,7 +8,7 @@
 <body>
 <h2>Documentary of my Computer Hardware through the Ages</h2>
 
-<h3>1998</h3>
+<h3>November 1998</h3>
 <p>
 Intel Pentium II @ 350MHz <br />
 64MB RAM <br />
@@ -17,27 +17,52 @@ Intel Pentium II @ 350MHz <br />
 </p>
 
 <h3>2004</h3>
+<p>
 Intel Celeron D @ 2.66GHz (Prescott) <br />
 256MB RAM <br />
 Integrated Intel Graphics <br />
 ~60GB HDD <br />
++512MB RAM (Winter 2005) <br />
++PCI Radeon 9250 128MB (Winter 2005)<br />
 </p>
-
-<h3>Winter 2005</h3>
-Intel Celeron D @ 2.66GHz (Prescott) <br />
-768MB RAM <br />
-PCI Radeon 9250 128MB<br />
-~60GB HDD <br />
 
 <h3>August 2006</h3>
+<p>
 AMD Athlon 64 X2 4200+ @ 2,2 GHz <br />
 1GB DDR2 533 <br />
+ASUS M2NPV-VM
 PCI-E Geforce 7600GT 256MB <br />
-160GB DD <br />
+160GB HDD <br />
 </p>
 
+<h3>June-August 2008</h3>
+<p>
+Intel Core 2 Quad Q6600 @3.4-3.6GHz <br />
+4GB DDR2 800 <br />
+EVGA 750i SLI FTW <br />
+PCI-E Nvidia Geforce 9800GX2 512MBx2 <br />
+640GB HDD <br />
+-EVGA 750i SLI FTW <br />
++ASUS P5Q-E <br />
++1TB HDD <br />
++2TB HDD <br />
+-PCI-E Nvidia Geforce 9800GX2 512MBx2 <br />
++PCI-E Nvidia Geforce 8400GS <br />
+-PCI-E Nvidia Geforce 8400GS <br />
++PCI-E AMD Radeon 6870 1GB <br />
+</p>
 
+<h3>December 2012</h3>
+<p>
+(Mini-ITX Build)<br />
+Intel i5 3570K @4.2GHz <br />
+16GB DDR3 1333 <br />
+MSI Z77IA-E53 <br />
+PCI-E Nvidia Geforce GTX 660 2GB <br />
+120GB SSD (SF 2218 based) <br />
+</p>
 <h6>This page created on August 28, 2012</h6>
+<h6>This page updated on December 21, 2012</h6>
 </body>
 
 </html>
